30e5d4d0d29e2f8cb931c5683d3b675c6994dd15
zhangtaozhi <644728909@qq.com>
2020-01-08 14:50:53 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("hello3 world");
+		System.out.println("hello2 world");		
+		System.out.println("hello1 world");
+		System.out.println("hello0 world");
+		
+		
+	}
+}
